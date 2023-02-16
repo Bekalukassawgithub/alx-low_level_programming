aab788accec3ec91b15abd1a9b32e05fe583cee3
bekalukassawgithub <bekalukassaw19@gmail.com>
2023-02-16 01:32:14 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line witj puts function
+ * Return : 0 (success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzel");
+return (0);
+}
