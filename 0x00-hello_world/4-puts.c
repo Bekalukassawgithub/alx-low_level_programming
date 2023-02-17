52a0ac377eb77df164ce7a83d93053f2d86514e0
bekalukassawgithub <bekalukassaw19@gmail.com>
2023-02-17 00:39:23 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that print a line with puts function
- * Return : i0(success)
+ * main - Entry
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-puts("programming is like building a multilingual puzzel");
+puts("\"programming is like building a multilingual puzzel");
 return (0);
 }
