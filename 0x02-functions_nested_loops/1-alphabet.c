c4404bd46681d3d4c87efc6d90f662608c1f102e
bekalukassawgithub <bekalukassaw19@gmail.com>
2023-02-21 08:37:29 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - entry point
+ * return: 0
+ */
+void print_alphabet(void)
+{
+	char c;
+for (c = 'a', c <= 'z', c++)
+{
+	_putchar(c);
+}
+_Putchar('\n')
+return (0);
+}
+
