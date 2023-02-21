807960180ae418bd8738b2825ba019a2e90bc0c7
bekalukassawgithub <bekalukassaw19@gmail.com>
2023-02-21 09:51:27 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - entery point
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+	 i = 0;
+	while (i < 0)
+	{
+		ch = 'z';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
